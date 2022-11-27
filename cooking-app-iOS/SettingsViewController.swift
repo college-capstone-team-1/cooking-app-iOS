83c0c6e3e8d42f894090507a97f222d8b93d22de
WS <blb138@naver.com>
2022-11-27 18:16:37 +0900

    Chore: 로그인/로그아웃 버튼 통합 및 회원가입 에러출력 구현 로그인 로그아웃 버튼을 하나로 통합, 회원가입 실패시 토스트메시지를 출력

--- cooking-app-iOS/SettingsViewController.swift
+++ cooking-app-iOS/SettingsViewController.swift
@@ -10,35 +10,44 @@ import FirebaseAuth
 
 class SettingsViewController: UIViewController {
 
+
     @IBOutlet weak var loginBtn: UIButton!
-    @IBOutlet weak var logoutBtn: UIButton!
     @IBOutlet weak var txtEmail: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //버튼 테두리 굵기
         loginBtn.layer.borderWidth = 2
-        logoutBtn.layer.borderWidth = 2
         //버튼 테두리 색상
         loginBtn.layer.borderColor = UIColor.black.cgColor
-        logoutBtn.layer.borderColor = UIColor.blue.cgColor
         
     }
     
-    
-    @IBAction func logoutBtn(_ sender: UIButton) {
-        
-        txtEmail.text = "로그인이 필요합니다"
-        try? Auth.auth().signOut()
-    }
-    
+
     @IBAction func loginBtn(_ sender: Any) {
         
-        let LoginSB = UIStoryboard(name: "Login", bundle: nil)
-        let LoginNVC = LoginSB.instantiateViewController(withIdentifier: "LOGINNAV")
-        LoginNVC.modalPresentationStyle = .fullScreen
-        self.present(LoginNVC, animated: true)
+        //로그인상태 체크
+        if Auth.auth().currentUser != nil {
+            //로그인 상태
+            
+            //로그아웃
+            try? Auth.auth().signOut()
+            //로그인버튼 변경
+            txtEmail.text = "로그인이 필요합니다"
+            loginBtn.layer.borderColor = UIColor.black.cgColor
+            loginBtn.setTitle("로그인 / 회원가입", for: .normal)
+            
+        } else{
+            //로그아웃 상태
+            
+            //로그인페이지로 이동
+            let LoginSB = UIStoryboard(name: "Login", bundle: nil)
+            let LoginNVC = LoginSB.instantiateViewController(withIdentifier: "LOGINNAV")
+
+            LoginNVC.modalPresentationStyle = .fullScreen
+            self.present(LoginNVC, animated: true)
+        }
         
     }
 
@@ -46,10 +55,16 @@ class SettingsViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         //화면이 다시 나타날 때 마다 로그인상태 체크
         if let user = Auth.auth().currentUser {
+            //로그인 상태
             txtEmail.text = user.email
+            loginBtn.layer.borderColor = UIColor.red.cgColor
+            loginBtn.setTitle("로그아웃", for: .normal)
         }
         else{
+            //로그아웃 상태
             txtEmail.text = "로그인이 필요합니다"
+            loginBtn.layer.borderColor = UIColor.black.cgColor
+            loginBtn.setTitle("로그인 / 회원가입", for: .normal)
         }
     }
     
--- cooking-app-iOS/SignUpViewController.swift
+++ cooking-app-iOS/SignUpViewController.swift
@@ -21,6 +21,20 @@ class SignUpViewController: UIViewController {
         
         Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPW.text!) { (result, error) in
             
+            if error != nil{
+                print("error: \(error?.localizedDescription)")
+                switch error?.localizedDescription {
+                    
+                case "The email address is already in use by another account.":
+                    self.showToast(message: "이미 등록된 이메일입니다")
+                    print("!")
+                    
+                default:
+                    self.showToast(message: "회원가입 실패")
+                }
+                return
+            }
+            
             guard let email = result?.user.email else {
                 
                 print("회원가입 실패")
@@ -31,4 +45,19 @@ class SignUpViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
+    
+    func showToast(message : String) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 100, y: self.view.frame.size.height-100, width: 200, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.5, delay: 0.1, options: .curveEaseOut, animations: { toastLabel.alpha = 0.0},
+                       completion: {(isCompleted) in toastLabel.removeFromSuperview() })
+    }
+    
 }
